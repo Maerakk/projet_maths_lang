6f113cdb256554e687292956d9dc83c4070b2b3a
Youllou <juless.barbier@gmail.com>
2021-12-14 17:51:18 +0100

    test naive

--- test/langModel/NaiveLanguageModelTest.java
+++ test/langModel/NaiveLanguageModelTest.java
@@ -1,20 +1,33 @@
 package langModel;
 
+import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 
 public class NaiveLanguageModelTest {
 
-    @Test
-    public void getLMOrder1() {
+
+    NgramCountsInterface ngram = new NgramCounts();
+    VocabularyInterface vocab = new Vocabulary();
+    LanguageModelInterface bigramm = new NaiveLanguageModel();
+
+    @Before
+    public void initiate() {
+        vocab.readVocabularyFile("lm/small_corpus/vocabulary1_in.txt");
+        ngram.scanTextFile("data/small_corpus/corpus_fr_train.txt",vocab,2);
+        bigramm.setNgramCounts(ngram,vocab);
     }
 
     @Test
-    public void setNgramCounts1() {
+    public void getLMOrder1() {
+        assertEquals(2,bigramm.getLMOrder());
     }
+
 
     @Test
     public void getNgramProb1() {
+        assertEquals();
     }
 
     @Test
