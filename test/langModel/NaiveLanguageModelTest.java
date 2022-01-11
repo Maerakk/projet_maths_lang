b7684c5f3e85b0cb91c1c8e31c752f85d6009a70
Youllou <juless.barbier@gmail.com>
2022-01-11 15:14:37 +0100

    naive test + laplace test

--- test/langModel/NaiveLanguageModelTest.java
+++ test/langModel/NaiveLanguageModelTest.java
@@ -21,16 +21,39 @@ public class NaiveLanguageModelTest {
 
     @Test
     public void getLMOrder1() {
+        // before the initiation, getLMOrder should return -1
+        assertEquals(-1,bigramm.getLMOrder());
+
+        initiate();
+        // after the initiation, it should return 2 since we work with bigramms
         assertEquals(2,bigramm.getLMOrder());
     }
 
 
     @Test
     public void getNgramProb1() {
-//        assertEquals();
+        // before initation, getNgramProb should return 0 for any value
+        assertEquals(Double.valueOf(0) , bigramm.getNgramProb("<s>"));
+
+        initiate();
+        // aftrer initiation, it should return 1 for <s>
+        assertEquals(Double.valueOf(1), bigramm.getNgramProb("<s>"));
+        // and the valid probability => "a b" doesn't exists
+        assertEquals(Double.valueOf(0), bigramm.getNgramProb("a b"));
+        //                          => "<s> Denis" should have a 1/14 probability
+        assertEquals(Double.valueOf(1.0/3),bigramm.getNgramProb("<s> denis"));
     }
 
     @Test
     public void getSentenceProb1() {
+        // before initiation, getSentenceProb should return 0 for any sentence
+        assertEquals(Double.valueOf(0),bigramm.getSentenceProb("<s> antoine écoute une chanson </s>"));
+
+        initiate();
+        // after initiation, it should return the valid probability
+        // => "a b c" does not exists so 0
+        assertEquals(Double.valueOf(0), bigramm.getSentenceProb("a b c"));
+        // => "<s> Antoine écoute une chanson </s>" 2/36
+        assertEquals(Double.valueOf(1.0/18),bigramm.getSentenceProb("<s> antoine écoute une chanson </s>"));
     }
 }
